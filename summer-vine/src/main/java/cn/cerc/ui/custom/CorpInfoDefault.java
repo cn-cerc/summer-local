1ab1e8762dbfb9a8284ed439ca759b342d96e10e
李远 <460289628@qq.com>
2021-07-10 04:51:54 +0000

    !53 改回session模式

--- summer-vine/src/main/java/cn/cerc/ui/custom/CorpInfoDefault.java
+++ summer-vine/src/main/java/cn/cerc/ui/custom/CorpInfoDefault.java
@@ -3,7 +3,6 @@ package cn.cerc.ui.custom;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
@@ -14,8 +13,8 @@ import cn.cerc.mis.services.MemoryBookInfo;
 import cn.cerc.ui.core.ICorpInfo;
 
 @Component
-//@Scope(WebApplicationContext.SCOPE_SESSION)
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(WebApplicationContext.SCOPE_SESSION)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CorpInfoDefault implements ICorpInfo {
     private Map<String, String> items = new ConcurrentHashMap<>();
 
