9644e9f5eb8b3798a927154b8cf1e901ebf72f20
张弓 <sz9214e@qq.com>
2021-10-11 10:23:54 +0800

    暂关闭此2项的编译警告

--- summer-vine/src/main/java/cn/cerc/ui/parts/UIFormHorizontal.java
+++ summer-vine/src/main/java/cn/cerc/ui/parts/UIFormHorizontal.java
@@ -2,7 +2,6 @@ package cn.cerc.ui.parts;
 
 import cn.cerc.ui.core.UIComponent;
 
-@Deprecated
 public class UIFormHorizontal extends UIFormSearch {
 
     public UIFormHorizontal(UIComponent owner) {
--- summer-vine/src/main/java/cn/cerc/ui/parts/UIFormVertical.java
+++ summer-vine/src/main/java/cn/cerc/ui/parts/UIFormVertical.java
@@ -1,6 +1,5 @@
 package cn.cerc.ui.parts;
 
-@Deprecated
 public class UIFormVertical extends UIFormEdit {
 
     public UIFormVertical(UIContent owner) {
