031e6d2d4ffc3a938cf0de470c15d1cb7da2be4e
张建红 <sz9214e@qq.com>
2022-01-22 00:30:55 +0000

    !106 调整包名、目录名，文件未变动

---
+++ summer-local-cn/src/test/java/cn/cerc/sms/YunpianSMSTest.java
@@ -0,0 +1,22 @@
+package cn.cerc.sms;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cn.cerc.local.sms.YunpianSMS;
+
+public class YunpianSMSTest {
+
+    private YunpianSMS yunpian;
+
+    @Before
+    public void setUp() {
+        yunpian = new YunpianSMS("4cf5608b2be57c8275ab67b9c1710797");
+    }
+
+    @Test
+    public void test() {
+        yunpian.send("15915438774", "【万事达】您的验证码是87616");
+    }
+
+}
