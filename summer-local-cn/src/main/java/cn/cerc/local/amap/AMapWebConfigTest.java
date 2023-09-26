7747bcbeced85f7f844d070bbc14f30bfb77ea93
HUANG Rongjun <l1091462907@gmail.com>
2023-09-27 00:17:51 +0800

    直接开启线程计算，取消线程排队一起开始

--- summer-local-cn/src/main/java/cn/cerc/local/amap/AMapWebConfigTest.java
+++ summer-local-cn/src/main/java/cn/cerc/local/amap/AMapWebConfigTest.java
@@ -8,30 +8,18 @@ import cn.cerc.db.redis.JedisFactory;
 import redis.clients.jedis.Jedis;
 
 public class AMapWebConfigTest {
-    private static final int THREAD_COUNT = 100000;
-    private static final CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
-
+    private static final int THREAD_COUNT = 50000;
     public static void main(String[] args) throws InterruptedException {
         Set<AMapWebConfig> set = new HashSet<>();
         for (int i = 0; i < THREAD_COUNT; i++) {
             new Thread(() -> {
-                try {
-                    latch.await();
-                    AMapWebConfig config = AMapWebConfig.getKey();
-                    set.add(config);
-                    if (set.size() > 1) {
-                        try (Jedis redis = JedisFactory.getJedis()) {
-                            redis.hset("itjun-thread", String.valueOf(config.hashCode()),
-                                    String.valueOf(Thread.currentThread().getId()));
-                        }
-                        System.out.println("stop");
-                        System.exit(0);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                AMapWebConfig config = AMapWebConfig.getKey();
+                set.add(config);
+                if (set.size() > 1) {
+                    System.out.println("stop" + set.size());
+                    System.exit(0);
                 }
             }).start();
-            latch.countDown();
         }
     }
 }
