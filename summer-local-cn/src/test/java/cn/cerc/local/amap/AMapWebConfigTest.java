10f8096c852ecf9a2d7d2e67d6d9b3500e9baac9
HUANG Rongjun <l1091462907@gmail.com>
2023-11-12 12:05:02 +0800

    add

--- summer-local-cn/src/test/java/cn/cerc/local/amap/AMapWebConfigTest.java
+++ summer-local-cn/src/test/java/cn/cerc/local/amap/AMapWebConfigTest.java
@@ -2,22 +2,29 @@ package cn.cerc.local.amap;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.List;
-
+import org.junit.Before;
 import org.junit.Test;
+
+import cn.cerc.db.core.ServerConfig;
 
 public class AMapWebConfigTest {
 
+    @Before
+    public void setup() {
+        ServerConfig.getInstance().setProperty("amap.web.svc.key", "a,b,c");
+    }
+
     @Test
     public void test_getKey() {
-        AMapWebConfig config = new AMapWebConfig(List.of("a", "b", "c"));
-        assertEquals("a", config.getNext());
-        assertEquals("b", config.getNext());
-        assertEquals("c", config.getNext());
+        assertEquals("a", AMapWebConfig.getKey());
+        assertEquals("b", AMapWebConfig.getKey());
+        assertEquals("c", AMapWebConfig.getKey());
 
-        assertEquals("a", config.getNext());
-        assertEquals("b", config.getNext());
-        assertEquals("c", config.getNext());
+        assertEquals("a", AMapWebConfig.getKey());
+        assertEquals("b", AMapWebConfig.getKey());
+        assertEquals("c", AMapWebConfig.getKey());
+
+        assertEquals("a", AMapWebConfig.getKey());
     }
 
 }
